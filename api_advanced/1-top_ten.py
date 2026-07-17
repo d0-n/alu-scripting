a4c803ab67c7eae7df102abcb7381134c1f5a40f
d0-n <d.marume@alustudent.com>
2026-07-17 16:45:38 +0200

    Fixed output

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,20 +4,16 @@ import requests
 
 
 def top_ten(subreddit):
-    """prints titles of first 10 hot posts"""
+    """prints top ten hot posts"""
     url = "https://www.reddit.com/r/{}/hot.json"
-    headers = {"User-Agent": "anything"}
-    try:
-        r = requests.get(
-            url.format(subreddit),
-            headers=headers,
-            allow_redirects=False,
-            params={"limit": 10}
-        )
-        if r.status_code != 200:
-            print(None)
-            return
+    r = requests.get(
+        url.format(subreddit),
+        headers={"User-Agent": "anything"},
+        allow_redirects=False,
+        params={"limit": 10}
+    )
+    if r.status_code == 200:
         for post in r.json()["data"]["children"]:
             print(post["data"]["title"])
-    except Exception:
-        print(None)
+    else:
+        print("None")
