42479223f47c3fb5436c25def6bb5504e89c95b4
d0-n <d.marume@alustudent.com>
2026-07-17 16:35:30 +0200

    Fixed output

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,14 +5,19 @@ import requests
 
 def top_ten(subreddit):
     """prints titles of first 10 hot posts"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10"
-    r = requests.get(
-        url.format(subreddit),
-        headers={"User-Agent": "alu:api_advanced:v1.0"},
-        allow_redirects=False
-    )
-    if r.status_code != 200:
+    url = "https://www.reddit.com/r/{}/hot.json"
+    headers = {"User-Agent": "anything"}
+    try:
+        r = requests.get(
+            url.format(subreddit),
+            headers=headers,
+            allow_redirects=False,
+            params={"limit": 10}
+        )
+        if r.status_code != 200:
+            print(None)
+            return
+        for post in r.json()["data"]["children"]:
+            print(post["data"]["title"])
+    except Exception:
         print(None)
-        return
-    for post in r.json()["data"]["children"]:
-        print(post["data"]["title"])
