063e63248049b7fed8e3b9417a50951b01f22f2b
d0-n <d.marume@alustudent.com>
2026-07-21 10:14:53 +0200

    fixed output

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """queries reddit"""
+import sys
+
 import requests
 
 
@@ -17,3 +19,7 @@ def top_ten(subreddit):
     children = data.get("children", [])
     for i in range(min(10, len(children))):
         print(children[i].get("data", {}).get("title"))
+
+
+if __name__ == "__main__":
+    top_ten(sys.argv[1])
